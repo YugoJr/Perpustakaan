6c25b2c72470bf9ea305a5a7f985020e8dcaf02e
ibni-abiyyu <24_ibni@student.smkti.net>
2025-10-13 14:28:38 +0800

    dri bni

--- app/model/user.php
+++ app/model/user.php
@@ -1,2 +1,28 @@
 <?php
-Require_once 'Config/Database.php';
+require_once "Config/Database.php";
+
+class User {
+    private $conn;
+    public function __construct() {
+        $db = new Database();
+        $this->conn = $db->connect();
+    }
+
+    public function register($username, $email, $password) {
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+        $query = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+        $stmt = $this->conn->prepare($query);
+        return $stmt->execute([$username, $email, $hash]);
+    }
+
+    public function login($email, $password) {
+        $stmt = $this->conn->prepare("SELECT * FROM users WHERE email = ?");
+        $stmt->execute([$email]);
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($user && password_verify($password, $user['password'])) {
+            return $user;
+        }
+        return false;
+    }
+}
+?>
